e8b0d9076d2743cb5db63a1cf7f87060832697f4
Luke Waldron <lukewaldron87@gmail.com>
2023-08-04 15:48:37 +0200

    added streamMovieEvents

--- src/main/java/com/waldron/springframework/netflux/controller/MovieController.java
+++ src/main/java/com/waldron/springframework/netflux/controller/MovieController.java
@@ -1,9 +1,10 @@
 package com.waldron.springframework.netflux.controller;
 
 import com.waldron.springframework.netflux.domain.Movie;
+import com.waldron.springframework.netflux.domain.MovieEvent;
 import com.waldron.springframework.netflux.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,10 @@ public class MovieController {
     @GetMapping
     Flux<Movie> getAllMovies(){
         return movieService.getAllMovies();
+    }
+
+    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<MovieEvent> streamMovieEvents(@PathVariable String id){
+        return movieService.streamMovieEvents(id);
     }
 }
--- src/main/java/com/waldron/springframework/netflux/service/MovieServiceImpl.java
+++ src/main/java/com/waldron/springframework/netflux/service/MovieServiceImpl.java
@@ -1,11 +1,15 @@
 package com.waldron.springframework.netflux.service;
 
 import com.waldron.springframework.netflux.domain.Movie;
+import com.waldron.springframework.netflux.domain.MovieEvent;
 import com.waldron.springframework.netflux.repositories.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +25,12 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Flux<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    @Override
+    public Flux<MovieEvent> streamMovieEvents(String id) {
+        return Flux.<MovieEvent>generate(movieEventSynchronousSink -> {
+            movieEventSynchronousSink.next(new MovieEvent(id, new Date()));
+        }).delayElements(Duration.ofSeconds(1));
     }
 }
